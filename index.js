dae59110a53b421db8db9bd144e6932eee118d09
NicoleOrmerod94 <147579584+NicoleOrmerod94@users.noreply.github.com>
2023-10-29 18:52:59 -0600

    completed

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a, b) {
+    return a + b;
+  }
+function subtract(a, b) {
+    return a - b;
+  }
+function add(a, b) {
+    return a + b;
+  }
+  
+function subtract(a, b) {
+    return a - b;
+  }
+  
+function multiply(a, b) {
+    return a * b;
+  }
+  
+function divide(a, b) {
+    if (b === 0) {
+      throw new Error("Division by zero is not allowed.");
+    }
+    return a / b;
+}
+  
+ function increment(n) {
+    return n + 1;
+}
+  
+function decrement(n) {
+    return n - 1;
+}
+
+function makeInt(n) {
+  const parsedInt = parseInt(n, 10);
+  return isNaN(parsedInt) ? NaN : parsedInt;
+}
+
+function preserveDecimal(n) {
+  const parsedFloat = parseFloat(n);
+  return isNaN(parsedFloat) ? NaN : parsedFloat;
+}
